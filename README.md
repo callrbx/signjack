abb5a90b2115a3495a47874d7e67e9bd4f81643a
SoulSaber1 <soulsaber1@gmail.com>
2017-04-27 19:19:00 -0400

    added scanning functionality

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # signjack
 Simple tool i made to replace pictures in unsecure BrightSign servers
+
+Leverages Flask, nmap, and good'ol web scraping to display pictures, and when you find one to replace, replace it and leave a backup of the original.
+
+I'm not a web dev; I'm a hacker.
--- signjack.py
+++ signjack.py
@@ -1,33 +1,78 @@
 #!/usr/bin/python2
 
-from flask import Flask, render_template, redirect, url_for
+#Setup correct imports
+import time, sys, re, socket, argparse
+
+try:
+  import nmap
+except:
+  print "install nmap library"
+  exit(-1)
+
+try:
+  from flask import Flask, render_template, redirect, url_for, request
+except:
+  print "install flask"
+  exit(-1)
+
+#add some sort of sudo check
+
+#The flask object our app runs on top of
 app = Flask(__name__)
 
+#Define the vendor id for BrightSign MAC
+bsmac = "c0:56:27"  #using Belkin, as i do not have access to sign
+
+#Global devices array; holds added device info
 devices = []
 
+#Determine our host ip; if not provided, use socket methods
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+host = ".".join(s.getsockname()[0].split(".")[:-1])+".0"
+s.close()
+
+#Map our index page
 @app.route("/")
 def index():
   global devices
   return render_template("index.html", devices=devices)
 
+#Add a device by hand; no scanning required
+@app.route("/add", methods=['POST'])
+def manual_add():
+  global devices
+  ip = request.args.get("ip")
+  loc = request.args.get("location")
+  devices.append((ip, loc))
+  return redirect(url_for("index")
 
+#Leverages nmap to scan the LAN for BrightSign MACs
 @app.route("/scan", methods=['POST'])
-def devScanButton():
+def dev_scan_button():
   global devices
-  devices = scanDevices()
+  devices = scan_devices()
   return redirect(url_for("index"))
 
+#Remove all devices from the global device list
 @app.route("/clear", methods=['POST'])
-def devClearButton():
+def dev_clear_button():
   global devices
   devices = []
   return redirect(url_for("index"))
 
-
-def scanDevices():
+#Nmap LAN for BrightSign MACs
+def scan_devices():
+  global host
   devices = []
-  devices.append(("192.168.1.1", "Grant"))
-  devices.append(("192.168.1.2", "Ike First Floor"))
+  nm = nmap.PortScanner()
+  print host
+  print "Scanning for network devices"
+  nm.scan(host+'/24', arguments='-O')
+  for h in nm.all_hosts():
+      if 'mac' in nm[h]['addresses']:
+          devices.append((nm[h]['addresses']['ipv4'], nm[h]['vendor'].values()[0]))
+
   return devices
 
 def main():
